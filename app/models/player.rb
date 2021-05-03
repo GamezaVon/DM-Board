ae548d187bcf1b93e1ede05b1e1600685e372fa5
Daniel Gamez <acocolito@gmail.com>
2021-05-03 07:37:33 -0700

    Sass modification for Exp bar

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,22 @@
 class Player < ApplicationRecord
+    $exp = [0,300,900,2700,6500,14000,23000,34000,48000,64000,85000,100000,120000,140000,165000,195000,225000,265000,305000,355000]
+    
+    def get_lvl
+        $exp.each do |lvl|
+            if experience <= lvl
+                return [$exp.find_index(lvl),$exp[$exp.find_index(lvl)-1],$exp[$exp.find_index(lvl)]]
+                break
+            end
+        end
+    end
+
+    def lvl_range
+        $exp.each do |lvl|
+            if experience <= lvl
+                return $exp[lvl]
+                break
+            end
+        end
+    end
+
 end
